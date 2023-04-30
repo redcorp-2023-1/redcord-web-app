75f0d377302a61cbbbc388dd74fe5c7d1f61329d
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2023-04-30 12:17:54 -0500

    feat: vue template

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import './style.css';
+import App from './App.vue';
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app');
