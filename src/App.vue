b4ee2ca7cafd0b7085c29a1b2a9bceb5d229839b
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-05-01 10:07:27 -0500

    create a feature Register

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import LogIn from "./components/LogIn.vue";
+import register from "./components/Register.vue";
+import Register from "./components/Register.vue";
 </script>
 
 <template>
-
+<Register/>
 </template>
 
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -5,15 +5,25 @@
       <p class="description">
         Descubre el mundo de la productividad para mejorar tu rutina laboral.
       </p>
-      <img src="../assets/icono2.png" alt="icon" class="icon">
+      <img src="../assets/icono3.png" alt="icon" class="icon">
     </div>
     <div class="right-section">
       <div class="header">
-        <a href="#" class="link">¿No eres miembro?</a>
-        <a href="#" class="link">Registrate ahora</a>
+        <a href="#" class="link">Ya eres miembro</a>
+        <a href="#" class="link">Iniciar sesión</a>
       </div>
-      <h2>Iniciar sesión Redcorp</h2>
+      <h2>Registrate en RedCorp</h2>
       <form class="form">
+        <div class="form-group-inline">
+          <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" required>
+          </div>
+          <div class="form-group">
+            <label for="apellido">Apellido:</label>
+            <input type="text" id="apellido" required>
+          </div>
+        </div>
         <div class="form-group">
           <label for="email">Dirección de correo electrónico:</label>
           <input type="email" id="email" required>
@@ -22,26 +32,40 @@
           <label for="password">Contraseña:</label>
           <input type="password" id="password" required>
         </div>
-        <button class="btn-login">Iniciar sesión</button>
-        <div class="forgot-password">
-          <a href="#" class="contra">¿Se te olvidó la contraseña?</a>
+        <div class="form-group">
+          <label for="confirm-password">Ingrese nuevamente la contraseña:</label>
+          <input type="password" id="confirm-password" required>
         </div>
+        <div class="form-group">
+          <label for="dni">DNI:</label>
+          <input type="text" id="dni" required>
+        </div>
+        <div class="form-group">
+          <label for="ruc">RUC:</label>
+          <input type="text" id="ruc" required>
+        </div>
+        <button class="btn-register">Continuar</button>
       </form>
+      <div class="verification">
+        <img src="../assets/icono4.png" alt="icon" class="message-icon">
+        <p>Se enviará un código de verificación de acceso a su correo ingresado</p>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "LogIn",
-};
+  name: "Register"
+}
 </script>
+
 <style scoped>
-/* estilos para el contenedor principal */
+  /* estilos para el contenedor principal */
 .container {
   display: flex;
   align-items: normal;
   height: 90vh;
-  background-color: #9F5454;
+  background-color: #7DA4CA;
   margin: 0 auto;
 }
 
@@ -92,20 +116,20 @@ export default {
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  margin-bottom: 100px;
+  margin-bottom: 10px;
 }
 
 .link {
   color: #665075;
   font-size: 1rem;
   text-decoration: none;
-  margin: 0 10px;
+  margin: 10px 0px;
 }
 
 h2 {
   color: #000000;
-  font-size: 2rem;
-  margin-bottom: 20px;
+  font-size: 1.2rem;
+  margin-bottom: 15px;
 }
 
 .form {
@@ -121,52 +145,71 @@ h2 {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  margin-bottom: 20px;
+  width: 80%;
+  margin-bottom: 5px;
 }
 
 label {
   color: #000000;
-  font-size: 1.1rem;
-  margin-bottom: 10px;
+  font-size: 1rem;
+  margin-bottom: 0 px;
   text-align-all: left;
-
 }
 
 input[type="email"],
-input[type="password"] {
-  width: 50%;
-  padding: 10px;
+input[type="password"],
+input[type="text"] {
+  width: 60%;
+  padding: 5px;
   border: none;
   border-radius: 20px;
-  background-color: #EFEEF5;
-  margin-bottom: 10px;
+  background-color: #F4F4F4;
+  margin-bottom:auto;
 }
 
-button.btn-login {
+button.btn-register {
   background-color: #C9725B;
   color: white;
   font-size: 1.2rem;
-  padding: 10px 50px;
+  padding: 10px 20px;
   border: none;
   border-radius: 20px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 }
 
-button.btn-login:hover {
-  background-color: #7D3B3B;
-}
-input {
-  color: #1a1a1a;
-}
-.contra {
-  color:#665075;
-  margin-top: 10px;
-  text-decoration: none;
-}
-.forgot-password {
-  margin-top: 15px;
-}
-</style>
 
+
+
+  .form-group-inline {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    margin-bottom: 5px;
+  }
+
+  .verification {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+  .message-icon {
+    margin-right: 10px;
+    width: 25px;
+    height: 25px;
+    object-fit: contain;
+  }
+
+  .verification p {
+    font-size: 14px;
+    margin: 0;
+  }
+  p{
+    color:black;
+  }
+
+</style>
