5aac7aa9678d9877dea9df52854e4b026e45ff9d
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-05-01 16:35:59 -0500

    sidebaragregado

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue';
 import index from './components/landingpagecomponents/index.vue';
+import SideBar from "./components/SideBar/SideBar.vue";
 </script>
 
 <template>
   <index></index>
+  <SideBar></SideBar>
 </template>
 
 <style scoped></style>
