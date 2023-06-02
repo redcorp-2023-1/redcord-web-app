925da806b501990046c1cbb3c0aa129064f5f45a
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-06-01 23:02:37 -0500

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<script setup>
-
-import HelloWorld from './components/HelloWorld.vue'
-import Verification from "./components/verification.vue";
-import LogIn from "./components/LogIn.vue";
-import Register from "./components/Register.vue";
-import SelectCargo from "./components/selectCargo.vue";
-</script>
-
-<template>
-  <register/>
-
 <script setup></script>
 
 <template>
