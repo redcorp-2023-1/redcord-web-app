372d94adaf4c473b06191a70e851e924ea82d56d
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-06-03 19:33:08 -0500

    Lista de usuarios agregado

--- src/components/trabajadores/lista_trabajadores.vue
+++ src/components/trabajadores/lista_trabajadores.vue
@@ -3,60 +3,52 @@
 <template>
     <main>
     <SideBar></SideBar>
-    
+
     <div class="card">
-        <pv-datatable :value="trabajadores" tableStyle="min-width: 50rem">
+        <pv-datatable :value="trabajadores" class="table">
             <pv-column field="id" header="ID"></pv-column>
-            
             <pv-column field="name" header="Nombre"></pv-column>
             <pv-column field="correo" header="Correo"></pv-column>
             <pv-column field="area" header="Área"></pv-column>
-            
         </pv-datatable>
 
         <div class="workers">
             <div v-for="Trabajador in trabajadores">
-            <router-link :to="{ name: 'Trabajador', params: { id: Trabajador.id, nombre: Trabajador.name, correo: Trabajador.correo, area: Trabajador.area, foto: Trabajador.foto } }">
-                <pv-button label="Ver perfil" style="display: block; padding-bottom: 9px;"/><p/>
-                
-            </router-link>
+                <button>Ver Perfil</button>
             </div>
-            
+
         </div>
 
-
-
     </div>
-        
+
+
     </main>
-   
+
 </template>
 
 <script>
 import {TrabajadorApiService} from '../services/trabajadores-api.service'
 import SideBar from '../SideBar/SideBar.vue';
+import Trabajador from "./trabajadores-card.vue";
 
 
 export default{
     name:"trabajadores",
-    components:{SideBar},
+    components:{Trabajador, SideBar},
     data(){
         return{
-            id:1,
             trabajadores:[],
             trabajadoresService: new TrabajadorApiService()
         }
     },
+
     beforeMount()
     {
-        this.id = this.$route.params.id
 
-        this.trabajadoresService.getAll().then((response)=>{
-            this.trabajadores = response.data
+        this.trabajadoresService.getAllTrabajadores().then((response)=>{
+            this.trabajadores = response.data;
         })
 
-
-       
     }
     
 }
@@ -64,16 +56,40 @@ export default{
 </script>
 
 <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
 main{
-    
+
     flex-direction: row;
     display: flex;
 
 }
 .card{
-    display: flex;
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: repeat(5,1fr);
+    gap: 0.1rem;
+}
+.table{
+    grid-column: 1/5;
+}
+td{
+    height: 60px;
+}
+
+button{
+    height:40px;
+    width: 100px;
+    margin-bottom:20px;
+    background:#C9725B;
+    color:white;
+    font-weight: bold;
+}
+button:hover{
+    background: #22242E;
 }
 
 .workers{
@@ -81,7 +97,10 @@ main{
     display: flex;
     flex-direction: column;
     text-align: center;
-    
 }
+    .p-column-title{
+       color: rgba(0,0,0,0.6);
+        border-bottom:1px solid rgba(0,0,0,0.6);
+    }
 
 </style>
